fix a bug in quspin --> eigen min computation

ed_dataset.py
@@ -1,26 +1,30 @@
-from src.utils_sparse_diagonalization import transverse_ising_sparse_simulator
 import argparse
+
+import matplotlib.pyplot as plt
 import numpy as np
+
+from src.utils_sparse_diagonalization import transverse_ising_sparse_simulator
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument(
-    "--n_dataset", type=int, help="# of istances in the dataset (default=150000)", default=15000
+    "--n_dataset",
+    type=int,
+    help="# of istances in the dataset (default=150000)",
+    default=15000,
 )
-parser.add_argument(
-    "--l", type=int, help="size of the chain ", default=16
-)
+parser.add_argument("--l", type=int, help="size of the chain ", default=16)
 parser.add_argument(
     "--j1",
     type=float,
     help="the coupling costant of the spin-spin interaction (default=1)",
-    default=-1.,
+    default=-1.0,
 )
 parser.add_argument(
     "--j2",
     type=float,
     help="the coupling costant of the spin-spin interaction (default=1)",
-    default=-1.,
+    default=-1.0,
 )
 
 parser.add_argument(
@@ -29,7 +33,6 @@ parser.add_argument(
     help="the maximum value of the transverse magnetic field (default=e)",
     default=np.e,
 )
-
 
 
 parser.add_argument(
@@ -70,6 +73,37 @@ parser.add_argument(
 
 args = parser.parse_args()
 np.random.seed(args.seed)
-hs=np.random.uniform(0,args.h_max,size=(args.n_dataset,args.l))
-file_name,es,hs,zs,fs_dens,fs,xs=transverse_ising_sparse_simulator(h_max=args.h_max,hs=hs,n_dataset=args.n_dataset,l=args.l,j1=args.j1,j2=args.j2,z_2=args.z2,file_name=args.file_name,pbc=args.pbc,check_2nn=args.check_2nn)
-np.savez(file_name,energy=es,potential=hs,density=zs,density_F=fs_dens,F=fs,magnetization_x=xs)
+print("n_dataset=", args.n_dataset)
+hs = np.random.uniform(0, args.h_max, size=(args.n_dataset, args.l))
+file_name, es, hs, zs, fs_dens, fs, xs = transverse_ising_sparse_simulator(
+    h_max=args.h_max,
+    hs=hs,
+    n_dataset=args.n_dataset,
+    l=args.l,
+    j1=args.j1,
+    j2=args.j2,
+    z_2=args.z2,
+    file_name=args.file_name,
+    pbc=args.pbc,
+    check_2nn=args.check_2nn,
+)
+np.savez(
+    file_name,
+    energy=es,
+    potential=hs,
+    density=zs,
+    density_F=fs_dens,
+    F=fs,
+    magnetization_x=xs,
+)
+
+
+# %%
+
+data = np.load("data/dataset_dmrg/l_64_h_2.71_ndata_10.npz")
+x = data["magnetization_x"]
+
+for i in range(10):
+    plt.plot(x[i])
+    plt.show()
+# %%

parallel_transverse_ising_correlation_map.py
@@ -1,15 +1,19 @@
+import argparse
+import os
+
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
+from scipy.fft import fft, ifft
 from torch import conj
-import matplotlib.pyplot as plt
-from scipy.fft import fft,ifft
 from tqdm import trange
+
+from src.utils import (nambu_diagonalization_ising_model,
+                       parallel_nambu_correlation_ising_model)
+
 # quantum transverse ising model 1d
 torch.manual_seed(42)
 torch.set_num_threads(10)
-import os
-import argparse
-from src.utils import nambu_diagonalization_ising_model, parallel_nambu_correlation_ising_model
 
 
 parser = argparse.ArgumentParser()
@@ -35,7 +39,6 @@ parser.add_argument(
     help="the maximum value of the transverse magnetic field (default=e)",
     default=np.e,
 )
-
 
 
 parser.add_argument(
@@ -74,26 +77,27 @@ parser.add_argument(
 args = parser.parse_args()
 
 
-hs=np.random.uniform(0,args.h_max,size=(args.n_dataset,args.l))
+hs = np.random.uniform(0, args.h_max, size=(args.n_dataset, args.l))
 
-ss_x,_,s_z=parallel_nambu_correlation_ising_model(nbatch=args.nbatch,l=args.l,j_coupling=args.j,hs=hs,device='cpu',name_file='none',pbc=args.pbc)
+ss_x, _, s_z = parallel_nambu_correlation_ising_model(
+    nbatch=args.nbatch, l=args.l, j_coupling=args.j, hs=hs, device='cpu', name_file='none', pbc=args.pbc)
 
 
-pbc_name=''
+pbc_name = ''
 if args.pbc:
-    pbc_name='pbc_'
+    pbc_name = 'pbc_'
 
-z2_name=''
+z2_name = ''
 if args.z2:
-    ss_x=np.append(ss_x,ss_x,axis=0)
-    s_z=np.append(s_z,-1*s_z,axis=0)
+    ss_x = np.append(ss_x, ss_x, axis=0)
+    s_z = np.append(s_z, -1*s_z, axis=0)
     indices = np.arange(s_z.shape[0])
     np.random.shuffle(indices)
-    ss_x=ss_x[indices]
-    s_z=s_z[indices]
-    z2_name='augmentation'
+    ss_x = ss_x[indices]
+    s_z = s_z[indices]
+    z2_name = 'augmentation'
 
-name=args.file_name+f'_h_{args.h_max}_'+f'n_{args.n_dataset}_'+f'l_{args.l}_'+pbc_name+f'j_{-1*args.j}'+z2_name
-np.savez('data/correlation_1nn/'+name,density=s_z,correlation=ss_x,potential=hs)
-
-
+name = args.file_name+f'_h_{args.h_max}_'+f'n_{args.n_dataset}_' + \
+    f'l_{args.l}_'+pbc_name+f'j_{-1*args.j}'+z2_name
+np.savez('data/correlation_1nn/'+name, density=s_z,
+         correlation=ss_x, potential=hs)

src/utils_sparse_diagonalization.py
@@ -1,102 +1,226 @@
 # import the sparse eigensolver
-from typing import Tuple,List,Optional
-from scipy.sparse.linalg import eigsh
-import numpy as np
-from scipy.sparse import csr_matrix
-import torch
-import matplotlib.pyplot as plt
-import quspin
-from quspin.operators import hamiltonian,quantum_operator,quantum_LinearOperator # Hamiltonians and operators
-from quspin.basis import spin_basis_1d # Hilbert space spin basis
 import argparse
+from typing import List, Optional, Tuple
+
+import matplotlib.pyplot as plt
+import numpy as np
+import quspin
+import torch
+from quspin.basis import spin_basis_1d  # Hilbert space spin basis
+from quspin.operators import hamiltonian  # Hamiltonians and operators
+from quspin.operators import quantum_LinearOperator, quantum_operator
+from scipy.sparse import csr_matrix
+from scipy.sparse.linalg import eigsh
 from tqdm import trange
 
-def density_of_functional_pbc(psi:np.array,l:int,basis:quspin.basis,j_1:float,j_2:float):
-    m={}
-    exp_m=[]
+
+def density_of_functional_pbc(
+    psi: np.array, l: int, basis: quspin.basis, j_1: float, j_2: float, check_2nn: bool
+):
+    m = {}
+    exp_m = []
     for i in range(l):
-        coupling=[[j_1,i,(i+1)%l],[j_2,i,(i+2)%l]]
-        op=['xx',coupling]
-        static=[op]
-        dynamic=[]
-        m=quantum_LinearOperator(static,basis=basis,dtype=np.float64,check_symm=False,check_herm=False,check_pcon=False)
+
+        if check_2nn:
+            coupling = [[j_1, i, (i + 1) % l], [j_2, i, (i + 2) % l]]
+        else:
+            coupling = [[j_1, i, (i + 1) % l]]
+        op = ["xx", coupling]
+        static = [op]
+        dynamic = []
+        m = hamiltonian(
+            static,
+            dynamic,
+            basis=basis,
+            dtype=np.float64,
+            check_symm=False,
+            check_herm=False,
+            check_pcon=False,
+        )
         exp_m.append(m.expt_value(psi))
     return exp_m
 
-def compute_magnetization(psi:np.array,l:int,basis:quspin.basis,direction:str):
-    #define the connection
-    m={}
-    exp_m=[]
+
+def compute_magnetization(psi: np.array, l: int, basis: quspin.basis, direction: str):
+    # define the connection
+    m = {}
+    exp_m = []
     for i in range(l):
-        coupling=[[1,i]]
-        op=[direction,coupling]
-        static=[op]
-        dynamic=[]
-        m=quantum_LinearOperator(static,basis=basis,dtype=np.float64,check_symm=False,check_herm=False,check_pcon=False)
+        coupling = [[1, i]]
+        op = [direction, coupling]
+        static = [op]
+        dynamic = []
+        m = hamiltonian(
+            static,
+            dynamic,
+            basis=basis,
+            dtype=np.float64,
+            check_symm=False,
+            check_herm=False,
+            check_pcon=False,
+        )
         exp_m.append(m.expt_value(psi))
     return exp_m
 
 
-def transverse_ising_sparse_simulator(h_max:int,hs:np.ndarray,n_dataset:int,l:int,j1:float,j2:float,pbc:bool,z_2:bool,file_name:str,check_2nn:bool)->Tuple[str,np.ndarray,np.ndarray,np.ndarray,np.ndarray,np.ndarray,np.ndarray]:
+def transverse_ising_sparse_simulator(
+    h_max: int,
+    hs: np.ndarray,
+    n_dataset: int,
+    l: int,
+    j1: float,
+    j2: float,
+    pbc: bool,
+    z_2: bool,
+    file_name: str,
+    check_2nn: bool,
+) -> Tuple[str, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
 
-    #file_name information
-    text_z2=''
-    text_field=f'_{h_max}_h'
+    # file_name information
+    text_z2 = ""
+    text_field = f"{h_max:.2f}_h"
 
-    hs=hs
-    #the basis of the representation
-    basis=spin_basis_1d(l)
+    hs = hs
+    # the basis of the representation
+    basis = spin_basis_1d(l)
 
     # the coupling terms
     if pbc:
-        j_1nn=[[j1,i,(i+1)%l] for i in range(l)] #pbc
+        j_1nn = [[j1, i, (i + 1) % l] for i in range(l)]  # pbc
         if check_2nn:
-            j_2nn=[[j2,i,(i+2)%l] for i in range(l)] #pbc
+            j_2nn = [[j2, i, (i + 2) % l] for i in range(l)]  # pbc
     else:
-        j_1nn=[[j1,i,(i+1)%l] for i in range(l)] #pbc
+        j_1nn = [[j1, i, (i + 1) % l] for i in range(l)]  # pbc
         if check_2nn:
-            j_2nn=[[j2,i,(i+2)%l] for i in range(l)] #pbc
+            j_2nn = [[j2, i, (i + 2) % l] for i in range(l)]  # pbc
 
+    for r in trange(n_dataset):
 
-    for i in trange(n_dataset):
-        h=[[hs[i,k],k] for k in range(l)] #external field
-        if check_2nn:    
-            static=[['xx',j_1nn],['xx',j_2nn],['z',h]]
+        h = [[hs[r, k], k] for k in range(l)]  # external field
+        if check_2nn:
+            static = [["xx", j_1nn], ["xx", j_2nn], ["z", h]]
         else:
-            static=[['xx',j_1nn],['z',h]]
-        dynamic=[]
-        ham=hamiltonian(static,dynamic,basis=basis,dtype=np.float64,check_symm=False,check_herm=False,check_pcon=False)
-        e,psi_0=ham.eigsh(k=1)
-        z=compute_magnetization(psi_0,l=l,basis=basis,direction='z')
-        x=compute_magnetization(psi_0,l=l,basis=basis,direction='x')
-        z=np.asarray(z)
-        x=np.asarray(x)
-        f_dens=density_of_functional_pbc(psi_0,l=l,basis=basis,j_1=j1,j_2=j2)
-        f_dens=np.asarray(f_dens)
-        f=e[0]/l - np.average(z*hs[i])
-        if i==0:
-            fs=f
-            zs=z.reshape(1,-1)
-            fs_dens=f_dens.reshape(1,-1)
-            es=e[0]/l
-            xs=x.reshape(1,-1)
+            static = [["xx", j_1nn], ["z", h]]
+        dynamic = []
+        ham = hamiltonian(
+            static,
+            dynamic,
+            basis=basis,
+            dtype=np.float64,
+            check_symm=False,
+            check_herm=False,
+            check_pcon=False,
+        )
+        e, psi_0 = ham.eigsh(k=1, sigma=-1000)
+
+        x = compute_magnetization(psi_0, l=l, basis=basis, direction="x")
+        z = compute_magnetization(psi_0, l=l, basis=basis, direction="z")
+        print("x=", x)
+        print("z=", z)
+        z = np.asarray(z)
+        x = np.asarray(x)
+        f_dens = density_of_functional_pbc(
+            psi_0, l=l, basis=basis, j_1=j1, j_2=j2, check_2nn=check_2nn
+        )
+        f_dens = np.asarray(f_dens)
+        f = e[0] / l - np.average(z * hs[r])
+        if r == 0:
+            fs = f
+            zs = z.reshape(1, -1)
+            fs_dens = f_dens.reshape(1, -1)
+            es = e[0] / l
+            xs = x.reshape(1, -1)
         else:
-            fs=np.append(fs,f)
-            zs=np.append(zs,z.reshape(1,-1),axis=0)
-            xs=np.append(xs,x.reshape(1,-1),axis=0)
-            fs_dens=np.append(fs_dens,f_dens.reshape(1,-1),axis=0)
-            es=np.append(es,e[0]/l)
+            fs = np.append(fs, f)
+            zs = np.append(zs, z.reshape(1, -1), axis=0)
+            xs = np.append(xs, x.reshape(1, -1), axis=0)
+            fs_dens = np.append(fs_dens, f_dens.reshape(1, -1), axis=0)
+            es = np.append(es, e[0] / l)
     if z_2:
-        text_z2='_augmentation'
-        fs=np.append(fs,fs,axis=0)
-        fs_dens=np.append(fs_dens,fs_dens,axis=0)
-        zs=np.append(zs,-1*zs,axis=0)
-        xs=np.append(xs,xs,axis=0)
+        text_z2 = "_augmentation"
+        fs = np.append(fs, fs, axis=0)
+        fs_dens = np.append(fs_dens, fs_dens, axis=0)
+        zs = np.append(zs, -1 * zs, axis=0)
+        xs = np.append(xs, xs, axis=0)
 
-    dir='data/dataset_1nn'
+    dir = "data/dataset_1nn/"
     if check_2nn:
-        dir='data/dataset_2nn'
+        dir = "data/dataset_2nn/"
 
-    file_name=dir+file_name+text_z2+f'_{l}_l_'+text_field+f'_{xs.shape[0]}_n'
+    file_name = (
+        dir + file_name + text_z2 + f"_{l}_l_" +
+        text_field + f"_{xs.shape[0]}_n"
+    )
 
-    return file_name,es,hs,zs,fs_dens,fs,xs
+    return file_name, es, hs, zs, fs_dens, fs, xs
+
+
+# def transverse_ising_sparse_simulator_sample(
+#     h_max: int,
+#     hs: np.ndarray,
+#     l: int,
+#     j1: float,
+#     j2: float,
+#     pbc: bool,
+#     file_name: str,
+#     check_2nn: bool,
+# ) -> Tuple[str, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+
+#     # file_name information
+#     text_z2 = ""
+#     text_field = f"{h_max:.2f}_h"
+
+#     hs = hs
+#     # the basis of the representation
+#     basis = spin_basis_1d(l)
+
+#     # the coupling terms
+#     if pbc:
+#         j_1nn = [[j1, i, (i + 1) % l] for i in range(l)]  # pbc
+#         if check_2nn:
+#             j_2nn = [[j2, i, (i + 2) % l] for i in range(l)]  # pbc
+#     else:
+#         j_1nn = [[j1, i, (i + 1) % l] for i in range(l)]  # pbc
+#         if check_2nn:
+#             j_2nn = [[j2, i, (i + 2) % l] for i in range(l)]  # pbc
+
+#     h = [[hs[k], k] for k in range(l)]  # external field
+#     if check_2nn:
+#         static = [["xx", j_1nn], ["xx", j_2nn], ["z", h]]
+#     else:
+#         static = [["xx", j_1nn], ["z", h]]
+#     dynamic = []
+#     ham = hamiltonian(
+#         static,
+#         dynamic,
+#         basis=basis,
+#         dtype=np.float64,
+#         check_symm=False,
+#         check_herm=False,
+#         check_pcon=False,
+#     )
+#     e, psi_0 = ham.eigsh(k=1, sigma=-1000)
+#     x = compute_magnetization(psi_0, l=l, basis=basis, direction="x")
+#     z = compute_magnetization(psi_0, l=l, basis=basis, direction="z")
+#     print("x=", x)
+#     print("z=", z)
+#     z = np.asarray(z)
+#     x = np.asarray(x)
+#     plt.plot(psi_0)
+#     plt.show()
+#     f_dens = density_of_functional_pbc(
+#         psi_0, l=l, basis=basis, j_1=j1, j_2=j2, check_2nn=check_2nn
+#     )
+#     f_dens = np.asarray(f_dens)
+#     f = e[0] / l - np.average(z * hs)
+#     fs = f
+#     zs = z.reshape(1, -1)
+#     fs_dens = f_dens.reshape(1, -1)
+#     es = e[0] / l
+#     xs = x.reshape(1, -1)
+
+#     # file_name = (
+#     #     dir + file_name + text_z2 + f"_{l}_l_" + text_field + f"_{xs.shape[0]}_n"
+#     # )
+
+#     return file_name, es, hs, zs, fs_dens, fs, xs

test.py
@@ -1,20 +1,20 @@
 #%%
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
-data=np.load('data/dataset_dmrg/l_64_h_2.71_ndata_10.npz')
+data = np.load("data/dataset_dmrg/l_64_h_2.71_ndata_10.npz")
 
-v=data['potential']
-z=data['density']
-e=data['energy']
-f=data['F']
+v = data["potential"]
+z = data["density"]
+e = data["energy"]
+f = data["F"]
 
 
 print(v.shape)
 #%%
 for i in range(10):
     plt.plot(v[i])
-    plt.plot(z[i],label=f'e={e[i]:.2f}')
+    plt.plot(z[i], label=f"e={e[i]:.2f}")
     plt.legend()
     plt.show()
 