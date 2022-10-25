32bb08e498d298ca7ff321b043ca67004c571757
emacosta95 <emanuelecosta95@hotmail.com>
2022-10-25 12:03:22 +0200

    complete the notebook about the H-K map

--- src/utils_sparse_diag.py
+++ src/utils_sparse_diag.py
@@ -11,27 +11,27 @@ from quspin.operators import hamiltonian  # Hamiltonians and operators
 from quspin.operators import quantum_LinearOperator, quantum_operator
 from scipy.sparse import csr_matrix
 from scipy.sparse.linalg import eigsh
-from tqdm.notebook import trange
+from tqdm import trange
 from quspin.tools.lanczos import lanczos_full, lanczos_iter, lin_comb_Q_T, expm_lanczos
 
 
-def lanczos_method(hamiltonian: quspin.operators.hamiltonian, l: int):
+def lanczos_method(hamiltonian: quspin.operators.hamiltonian, basis: quspin.basis):
     """Quspin Code for the Lanczos method --> http://weinbe58.github.io/QuSpin/examples/example20.html#example20-label"""
     ###### apply Lanczos
     # initial state for Lanczos algorithm
-    v0 = np.random.normal(0, 1, size=l)
-    v0 /= np.linalg.norm(v0)
+    v0 = np.random.normal(0, 1, size=basis.Ns)
+    v0 = v0 / np.linalg.norm(v0)
     #
-    m_GS = 50  # Krylov subspace dimension
+    m_GS = 10  # Krylov subspace dimension
     #
     # Lanczos finds the largest-magnitude eigenvalues:
-    E, V, Q_T = lanczos_full(hamiltonian, v0, m_GS, full_ortho=False)
+    e, v, q_t = lanczos_full(hamiltonian, v0, m_GS)
     #
     #
     # compute ground state vector
-    psi_GS_lanczos = lin_comb_Q_T(V[:, 0], Q_T)
+    psi_GS_lanczos = lin_comb_Q_T(v[:, 0], q_t)
 
-    return E, psi_GS_lanczos
+    return e, psi_GS_lanczos
 
 
 def density_of_functional_pbc(
@@ -160,6 +160,89 @@ def transverse_ising_sparse_DFT(
         )
         e, psi_0 = ham.eigsh(k=1)  # , sigma=-1000)
 
+        z = compute_magnetization(psi_0, l=l, basis=basis, direction="z")
+
+        z = np.asarray(z)
+        f_dens = density_of_functional_pbc(
+            psi_0, l=l, basis=basis, j_1=j1, j_2=j2, check_2nn=check_2nn
+        )
+        f_dens = np.asarray(f_dens)
+        if r == 0:
+            zs = z.reshape(1, -1)
+            fs_dens = f_dens.reshape(1, -1)
+            es = e
+        else:
+            zs = np.append(zs, z.reshape(1, -1), axis=0)
+            fs_dens = np.append(fs_dens, f_dens.reshape(1, -1), axis=0)
+            es = np.append(es, e)
+    if z_2:
+        text_z2 = "_augmentation"
+        fs_dens = np.append(fs_dens, fs_dens, axis=0)
+        zs = np.append(zs, -1 * zs, axis=0)
+
+    dir = "data/dataset_1nn/"
+    if check_2nn:
+        dir = "data/dataset_2nn/"
+
+    file_name = (
+        dir + file_name + text_z2 + f"_{l}_l_" + text_field + f"_{fs_dens.shape[0]}_n"
+    )
+
+    return file_name, hs, zs, fs_dens, es
+
+
+def transverse_ising_sparse_DFT_lanczos_method(
+    h_max: int,
+    hs: np.ndarray,
+    n_dataset: int,
+    l: int,
+    j1: float,
+    j2: float,
+    pbc: bool,
+    z_2: bool,
+    file_name: str,
+    check_2nn: bool,
+    eps_breaking: float,
+) -> Tuple[str, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+
+    # file_name information
+    text_z2 = ""
+    text_field = f"{h_max:.2f}_h"
+
+    hs = hs
+    # the basis of the representation
+    basis = spin_basis_1d(l)
+
+    # the coupling terms
+    if pbc:
+        j_1nn = [[j1, i, (i + 1) % l] for i in range(l)]  # pbc
+        if check_2nn:
+            j_2nn = [[j2, i, (i + 2) % l] for i in range(l)]  # pbc
+    else:
+        j_1nn = [[j1, i, (i + 1) % l] for i in range(l)]  # pbc
+        if check_2nn:
+            j_2nn = [[j2, i, (i + 2) % l] for i in range(l)]  # pbc
+
+    for r in trange(n_dataset):
+
+        h = [[hs[r, k], k] for k in range(l)]  # external field
+        eps_h = [[eps_breaking, k] for k in range(l)]
+        if check_2nn:
+            static = [["xx", j_1nn], ["xx", j_2nn], ["z", h]]  # , ["x", eps_h]]
+        else:
+            static = [["xx", j_1nn], ["z", h], ["x", eps_h]]
+        dynamic = []
+        ham = hamiltonian(
+            static,
+            dynamic,
+            basis=basis,
+            dtype=np.float64,
+            check_symm=False,
+            check_herm=False,
+            check_pcon=False,
+        )
+        # e, psi_0 = ham.eigsh(k=1)  # , sigma=-1000)
+        e, psi_0 = lanczos_method(hamiltonian=ham, basis=basis)
         z = compute_magnetization(psi_0, l=l, basis=basis, direction="z")
 
         z = np.asarray(z)
@@ -389,8 +472,8 @@ def transverse_ising_sparse_h_k_mapping_check_lanczos_method(
             check_herm=False,
             check_pcon=False,
         )
-        #e, psi_0 = ham.eigsh(k=1)  # , sigma=-1000)
-        e,psi_0=lanczos_method(hamiltonian=hamiltonian,l=l)
+        # e, psi_0 = ham.eigsh(k=1)  # , sigma=-1000)
+        e, psi_0 = lanczos_method(hamiltonian=ham, basis=basis)
 
         zz = compute_correlation(psi_0, l=l, basis=basis, direction="zz")
         z = compute_magnetization(psi_0, l=l, basis=basis, direction="z")
