c20fa9909341611858329cba7c34f09a865d9d8b
emacosta95 <emanuelecosta95@hotmail.com>
2022-10-28 16:42:43 +0200

    change the structure of dmrg_dataset.jl

--- dmrg_dataset.jl
+++ dmrg_dataset.jl
@@ -2,16 +2,18 @@
 using LinearAlgebra
 include("src/utils_dmrg.jl")
 using ITensors
-
+using Distributions
+using Random
+using ProgressBars
+# fix the number of threads
 BLAS.set_num_threads(10)
 # parameters
 seed=125
-linkdims=40
-sweep=30
-n=[16,32,64,128,256]
+linkdims=70
+sweep=20
+n=[24,48,96]
 j_coupling=-1.
-hmaxs=7.2
-nlinspace=5
+hmaxs=5.44
 #hmaxs=LinRange(0.1,12.,nlinspace) # for studying the phase transition
 ndata=100
 two_nn=true
@@ -21,7 +23,37 @@ eps_breaking=0.
 # how to implement a string
 # format
 
-for i=1:nlinspace
-    namefile="data/dataset_2nn/test_unet_periodic_2nn_$(n[i])_l_$(hmaxs)_h_$(ndata)_n.npz"
-    dmrg_nn_ising(seed,linkdims,sweep,n[i],j_coupling,j_coupling,hmaxs,ndata,eps_breaking,namefile,two_nn)
+#fix the seed
+Random.seed!(seed)
+
+# different sizes
+for j=1:3
+        #name file
+        namefile="data/dataset_2nn/test_unet_periodic_2nn_$(n[j])_l_$(hmaxs)_h_$(ndata)_n.npz"
+        e_tot = zeros(Float64,(ndata))
+        v_tot = zeros(Float64,(ndata,n[j]))
+        f_tot=zeros(Float64,(ndata))
+        dens_f_tot= zeros(Float64,(ndata,n[j]))
+        z_tot= zeros(Float64,(ndata,n[j]))
+        x_tot=zeros(Float64,(ndata,n[j]))
+        xxs=zeros(Float64,(ndata,n[j],n[j]))
+        for i=tqdm(1:ndata)
+
+                # initialize the field
+                h=rand(Uniform(0.,hmaxs),n[j])
+                energy,potential,z,x,dens_f,f,xx=dmrg_nn_ising(seed,linkdims,sweep,n[j],j_coupling,j_coupling,hmaxs,eps_breaking,namefile,two_nn,h)
+                
+                # cumulate
+                e_tot[i]=energy
+                v_tot[i,:]=potential
+                z_tot[i,:]=z
+                x_tot[i,:]=x
+                dens_f_tot[i,:]=dens_f
+                f_tot[i]=f
+                xxs[i,:,:]=xx
+
+                #save
+                npzwrite(namefile, Dict("density" => z_tot, "energy" => e_tot, "F" => f_tot,"density_F"=> dens_f_tot,"potential"=>v_tot,"magnetization_x"=>x_tot,
+                "correlation"=>xxs))
+        end
 end
--- src/utils_dmrg.jl
+++ src/utils_dmrg.jl
@@ -6,10 +6,10 @@ using Plots
 using NPZ
 using ProgressBars
 
-function dmrg_nn_ising(seed::Int64,linkdims::Int64,sweep::Int64,n::Int64,j_1::Float64,j_2::Float64,h_max::Float64,ndata::Int64,eps_breaking::Float64,namefile::String,two_nn::Bool)
+function dmrg_nn_ising(seed::Int64,linkdims::Int64,sweep::Int64,n::Int64,j_1::Float64,j_2::Float64,h_max::Float64,eps_breaking::Float64,namefile::String,two_nn::Bool,hs::Array{Float64})
 
     #fix the seed
-    Random.seed!(seed)
+    #Random.seed!(seed)
 
     #fix the representation
     sites=siteinds("S=1/2",n) #fix the basis representation
@@ -49,102 +49,74 @@ function dmrg_nn_ising(seed::Int64,linkdims::Int64,sweep::Int64,n::Int64,j_1::Fl
     # z_tot= [Vector{Float64}() for _ in 1:ndata]
     
     # alternative
-    e_tot = zeros(Float64,(ndata))
-    v_tot = zeros(Float64,(ndata,n))
-    f_tot=zeros(Float64,(ndata))
-    dens_f_tot= zeros(Float64,(ndata,n))
-    z_tot= zeros(Float64,(ndata,n))
-    x_tot=zeros(Float64,(ndata,n))
-    xxs=zeros(Float64,(ndata,n,n))
     #create the dataset
-    for i=tqdm(1:ndata)
-        #external potential
-        ham_ext=OpSum()
-        potential = zeros(Float64,n)
-        for j=1:n
-            h_i=rand(Uniform(0.,h_max))
-            potential[j]=h_i
-            ham_ext+=2*h_i,"Sz",j # external random field
-        end
-
-        for j=1:n
-            ham_ext+=2*eps_breaking,"Sx",j # to
-        end 
-        #ham_ext+=2*eps_breaking,"Sx",1
-        #fix the invariance problem
-        #end
-        # collect the external
-        # field
-        #push!(v_tot,potential)
-
-        hamiltonian=ham_0+ham_ext
-
-        # initialize the hamiltonian
-        # as matrix product operator
-        h=MPO(hamiltonian,sites) #define the hamiltonian
-        psi0=randomMPS(sites,linkdims) #initialize the product state
-
-    
-        #fix the sweeps
-        sweeps = Sweeps(sweep)
-        setmaxdim!(sweeps, 10,20,100,100,200)
-        setcutoff!(sweeps, 1E-10)
-
-        # energy values
-        energy, psi = dmrg(h,psi0, sweeps,outputlevel=0)
-
-        #compute the transverse magnetization and the density functional per site 
-        z=2*expect(psi,"Sz")
-        x=2*expect(psi,"Sx")
-        
-        xx=4*correlation_matrix(psi,"Sx","Sx")
-        x_1nn=zeros(Float64,(n))
-        x_2nn=zeros(Float64,(n))   
-        for j=1:n
-            if j==n 
-                # push!(x_1nn,xx[j,1])
-                # push!(x_,xx[j,2])
-                x_1nn[j]=xx[j,1]
-                x_2nn[j]=xx[j,2]
-            elseif j==n-1
-                # push!(x_1nn,xx[j,j+1])
-                # push!(x_2nn,xx[j,1])
-                x_1nn[j]=xx[j,j+1]
-                x_2nn[j]=xx[j,1]
-            else
-                # push!(x_1nn,xx[j,j+1])
-                # push!(x_2nn,xx[j,j+2])
-                x_1nn[j]=xx[j,j+1]
-                x_2nn[j]=xx[j,j+2]
-            end
-        end
-        if two_nn
-            dens_f=j_coupling*(x_1nn+x_2nn)
-        else
-            dens_f=j_coupling*x_1nn
-        end
-        f=energy/n-dot(potential,z)/n
-        print(length(f))
-
-        print("type of energy/n ",typeof(potential),"\n")
-        # push!(e_tot,energy/n)
-        # push!(z_tot,z)
-        # push!(dens_f_tot,dens_f)
-        # push!(f_tot,f)
-        # push!(v_tot,potential)
-        
-        # alternative method
-        e_tot[i]=energy/n
-        v_tot[i,:]=potential
-        z_tot[i,:]=z
-        x_tot[i,:]=x
-        dens_f_tot[i,:]=dens_f
-        f_tot[i]=f
-        xxs[i,:,:]=xx
-        
-        npzwrite(namefile, Dict("density" => z_tot, "energy" => e_tot, "F" => f_tot,"density_F"=> dens_f_tot,"potential"=>v_tot,"magnetization_x"=>x_tot,
-        "correlation"=>xxs))
-
+    #for i=tqdm(1:ndata)
+    #external potential
+    ham_ext=OpSum()
+    for j=1:n
+        ham_ext+=2*hs[j],"Sz",j # external random field
     end
 
+    for j=1:n
+        ham_ext+=2*eps_breaking,"Sx",j # to
+    end 
+    #ham_ext+=2*eps_breaking,"Sx",1
+    #fix the invariance problem
+    #end
+    # collect the external
+    # field
+    #push!(v_tot,potential)
+
+    hamiltonian=ham_0+ham_ext
+
+    # initialize the hamiltonian
+    # as matrix product operator
+    h=MPO(hamiltonian,sites) #define the hamiltonian
+    psi0=randomMPS(sites,linkdims) #initialize the product state
+
+
+    #fix the sweeps
+    sweeps = Sweeps(sweep)
+    setmaxdim!(sweeps,linkdims)
+    setcutoff!(sweeps, 1E-10)
+
+    # energy values
+    energy, psi = dmrg(h,psi0, sweeps,outputlevel=0)
+
+    #compute the transverse magnetization and the density functional per site 
+    z=2*expect(psi,"Sz")
+    x=2*expect(psi,"Sx")
+    
+    xx=4*correlation_matrix(psi,"Sx","Sx")
+    x_1nn=zeros(Float64,(n))
+    x_2nn=zeros(Float64,(n))   
+    for j=1:n
+        if j==n 
+            # push!(x_1nn,xx[j,1])
+            # push!(x_,xx[j,2])
+            x_1nn[j]=xx[j,1]
+            x_2nn[j]=xx[j,2]
+        elseif j==n-1
+            # push!(x_1nn,xx[j,j+1])
+            # push!(x_2nn,xx[j,1])
+            x_1nn[j]=xx[j,j+1]
+            x_2nn[j]=xx[j,1]
+        else
+            # push!(x_1nn,xx[j,j+1])
+            # push!(x_2nn,xx[j,j+2])
+            x_1nn[j]=xx[j,j+1]
+            x_2nn[j]=xx[j,j+2]
+        end
+    end
+    if two_nn
+        dens_f=j_coupling*(x_1nn+x_2nn)
+    else
+        dens_f=j_coupling*x_1nn
+    end
+    f=energy/n-dot(hs,z)/n
+    #print(length(f))
+
+    #print("type of energy/n ",typeof(h),"\n")    
+    # alternative method
+    return energy/n, hs,z,x,dens_f,f,xx
 end
